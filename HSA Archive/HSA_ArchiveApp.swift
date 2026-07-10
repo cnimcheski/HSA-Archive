6969af49f28103187e2b2603c3ded17457db840e
cnimcheski <cnimcheski@hotmail.com>
2026-07-09 21:34:57 -0700

    Initial Commit

---
+++ HSA Archive/HSA_ArchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  HSA_ArchiveApp.swift
+//  HSA Archive
+//
+//  Created by Steve Nimcheski on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct HSA_ArchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HSA ArchiveTests/HSA_ArchiveTests.swift
@@ -0,0 +1,17 @@
+//
+//  HSA_ArchiveTests.swift
+//  HSA ArchiveTests
+//
+//  Created by Steve Nimcheski on 7/9/26.
+//
+
+import Testing
+@testable import HSA_Archive
+
+struct HSA_ArchiveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
