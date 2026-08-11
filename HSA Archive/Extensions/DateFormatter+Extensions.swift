6126fb7acb44c0f3de765e754aa25de4e0ee2727
cnimcheski <cnimcheski@hotmail.com>
2026-08-11 10:31:12 -0700

    Fixed date formatting time zone bug

--- HSA Archive/Extensions/DateFormatter+Extensions.swift
+++ HSA Archive/Extensions/DateFormatter+Extensions.swift
@@ -13,7 +13,6 @@ extension DateFormatter {
         let formatter = DateFormatter()
         formatter.calendar = Calendar(identifier: .iso8601)
         formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter
     }()
